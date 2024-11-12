4e9a80bc7952b6c9a22ced0aeb2ed20ee7665689
crazyman222214 <schadejr617@gmail.com>
2024-11-11 23:48:48 -0700

    Initial Startup

---
+++ Assets/Scripts/MaterialController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MaterialController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("hola papi");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
